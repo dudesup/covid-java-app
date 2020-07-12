2608992783e19ada6cc7cd8d6f1b7c05c4799642
Daria Supińska <dariasupinska@gmail.com>
2020-07-12 10:08:18 +0200

    "Crud Repository"

--- src/main/java/com/sda/demo/model/CountryStatistics.java
+++ src/main/java/com/sda/demo/model/CountryStatistics.java
@@ -1,9 +1,17 @@
 package com.sda.demo.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+@NoArgsConstructor
+@Getter
+@Setter
 
 @Entity
 @Table(name="country_statistics")
@@ -16,40 +24,11 @@ public class CountryStatistics {
     private int deaths;
     private int recoverred;
 
-    public CountryStatistics() {
-    }
 
     public CountryStatistics(int active, int deaths, int recoverred){
-
         this.active=active;
         this.deaths=deaths;
         this.recoverred=recoverred;
-
-    }
-
-
-    public void setActive(int active) {
-        this.active = active;
-    }
-
-    public void setDeaths(int deaths) {
-        this.deaths = deaths;
-    }
-
-    public void setRecoverred(int recoverred) {
-        this.recoverred = recoverred;
-    }
-
-    public int getActive() {
-        return active;
-    }
-
-    public int getDeaths() {
-        return deaths;
-    }
-
-    public int getRecoverred() {
-        return recoverred;
     }
 
 }
--- src/main/java/com/sda/demo/repository/CountryRepository.java
+++ src/main/java/com/sda/demo/repository/CountryRepository.java
@@ -1,26 +1,11 @@
 package com.sda.demo.repository;
 
 import com.sda.demo.model.CountryStatistics;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Repository
-public class CountryRepository {
-    private Map<String, CountryStatistics> statisticsMap;
-
-    public CountryRepository(){
-        this.statisticsMap=new HashMap<>();
-        statisticsMap.put("pl", new CountryStatistics(10000, 10, 1));
-        statisticsMap.put("de", new CountryStatistics(2124234, 1132, 4423));
-    }
-
-    public CountryStatistics getCountry(String countryCode){
-        return statisticsMap.get(countryCode);
-    }
-
-    public Map<String, CountryStatistics> findAll(){
-        return statisticsMap;
-    }
+public interface CountryRepository  extends CrudRepository<CountryStatistics, String> {
+    
 }
